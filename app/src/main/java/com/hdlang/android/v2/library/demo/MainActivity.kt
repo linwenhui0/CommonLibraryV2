000f14a8f64280f99735326e11b8c0b426476f58
linwenhui <lwh8278222@gmail.com>
2022-06-11 18:54:11 +0800

    feat:初始化工程

---
+++ app/src/main/java/com/hdlang/android/v2/library/demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hdlang.android.v2.library.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
